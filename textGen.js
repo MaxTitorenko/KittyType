c15cffa8bbe38b9775d7c1058c72c49ea2076ad8
MaxTitorenko <72975996+MaxTitorenko@users.noreply.github.com>
2022-11-10 20:14:43 +0200

    Update textGen.js

--- textGen.js
+++ textGen.js
@@ -456,6 +456,22 @@ function timing(time){
             xVal = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60]
         }
 
+        if(diffEasy.checked){
+            difficulty.textContent = "Easy"
+        }else if(diffMedium.checked){
+            difficulty.textContent = "Medium"
+        }else if(diffHard.checked){
+            difficulty.textContent = "Hard"
+        }
+
+        if(words.checked){
+            mode.textContent = "Words"
+        }else if(letters.checked){
+            mode.textContent = "Letters"
+        }else if(numbers.checked){
+            mode.textContent = "Numbers"
+        }
+                               
         wpm.textContent = wpmVal
         rawWpm.textContent = rawVal
 
